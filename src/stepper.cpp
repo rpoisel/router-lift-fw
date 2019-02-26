eb86525ccf712a44f92c5995f41b8cc0655ffacd
Rainer Poisel <rainer.poisel@gmail.com>
2019-02-26 20:21:57 +0100

    Stepper driver simplified

--- src/stepper.cpp
+++ src/stepper.cpp
@@ -2,59 +2,42 @@
 
 #include <Arduino.h>
 
-void StepperMoving::execute(FSM* fsm)
-{
-  if (!this->stepsLeft)
-  {
-    fsm->setState(stepper->getIdleState());
-  }
-  auto now = micros();
-  if (now - this->lastStepTime < stepper->getStepDelay())
-  {
-    return;
-  }
-
-  this->doStep();
-  this->lastStepTime = now;
-
-  this->stepsLeft += (this->stepsLeft > 0) ? -1 : 1;
-}
-
-void StepperMoving::reset(int32_t numberOfSteps)
-{
-  this->stepsLeft = numberOfSteps;
-}
-
-void StepperMoving::doStep() const
-{
-  digitalWrite(stepper->getDirectionPin(), this->stepsLeft > 0 ? HIGH : LOW);
-  digitalWrite(stepper->getStepPin(), HIGH);
-  delayMicroseconds(1);
-  digitalWrite(stepper->getStepPin(), LOW);
-}
-
 Stepper::Stepper(uint32_t numberOfSteps, uint8_t directionPin, uint8_t stepPin)
-    : fsm(), stepDelay(0), stateIdle(), stateMoving(this), pinCount(2),
-      numberOfSteps(numberOfSteps), directionPin(directionPin), stepPin(stepPin)
+    : numberOfSteps(numberOfSteps), directionPin(directionPin), stepPin(stepPin), stepDelay(0)
 {
-  fsm.setState(&stateIdle);
-  pinMode(this->directionPin, OUTPUT);
-  pinMode(this->stepPin, OUTPUT);
+  pinMode(directionPin, OUTPUT);
+  pinMode(stepPin, OUTPUT);
   setSpeed(100);
 }
 
 void Stepper::update()
 {
-  fsm.update();
+  if (!stepsLeft)
+  {
+    return;
+  }
+  auto now = micros();
+  if (now - lastStepTime < stepDelay)
+  {
+    return;
+  }
+
+  digitalWrite(directionPin, stepsLeft > 0 ? HIGH : LOW);
+  digitalWrite(stepPin, HIGH);
+  delayMicroseconds(1);
+  digitalWrite(stepPin, LOW);
+
+  lastStepTime = now;
+
+  stepsLeft += (stepsLeft > 0) ? -1 : 1;
 }
 
 void Stepper::setSpeed(uint32_t whatSpeed)
 {
-  this->stepDelay = 60L * 1000L * 1000L / (this->numberOfSteps * whatSpeed);
+  stepDelay = 60L * 1000 * 1000 / (numberOfSteps * whatSpeed);
 }
 
 void Stepper::step(uint32_t numberOfSteps)
 {
-  stateMoving.reset(numberOfSteps);
-  fsm.setState(&stateMoving);
+  stepsLeft = numberOfSteps;
 }
--- src/stepper.h
+++ src/stepper.h
@@ -1,37 +1,7 @@
 #ifndef STEPPER_H
 #define STEPPER_H
 
-#include <FSM.h>
-
 #include <stdint.h>
-
-class Stepper;
-
-class StepperMoving : public State
-{
-  public:
-  StepperMoving(Stepper* stepper) : stepper(stepper), stepsLeft(0), lastStepTime(0)
-  {
-  }
-  void execute(FSM* fsm);
-  void reset(int32_t numberOfSteps);
-
-  private:
-  void doStep() const;
-
-  Stepper* stepper;
-  int32_t stepsLeft;
-  unsigned long lastStepTime;
-};
-
-class StepperIdle : public State
-{
-  public:
-  StepperIdle() = default;
-  inline void execute(FSM* fsm)
-  {
-  }
-};
 
 class Stepper
 {
@@ -40,53 +10,15 @@ class Stepper
 
   void setSpeed(uint32_t whatSpeed);
   void step(uint32_t numberOfSteps);
-
-  inline unsigned long getStepDelay() const
-  {
-    return this->stepDelay;
-  }
-  constexpr static int version()
-  {
-    return 6;
-  }
   void update();
 
-  inline uint8_t getPinCount() const
-  {
-    return pinCount;
-  }
-  inline uint32_t getNumberOfSteps() const
-  {
-    return numberOfSteps;
-  }
-  inline uint8_t getDirectionPin() const
-  {
-    return directionPin;
-  }
-  inline uint8_t getStepPin() const
-  {
-    return stepPin;
-  }
-  inline State* getIdleState()
-  {
-    return &stateIdle;
-  }
-  inline State* getMovingState()
-  {
-    return &stateMoving;
-  }
-
   private:
-  FSM fsm;
-
-  unsigned long stepDelay;
-  StepperIdle stateIdle;
-  StepperMoving stateMoving;
-
-  uint8_t const pinCount;
   uint32_t const numberOfSteps;
   uint8_t const directionPin;
   uint8_t const stepPin;
+  unsigned long stepDelay;
+  int32_t stepsLeft;
+  unsigned long lastStepTime;
 };
 
 #endif /* STEPPER_H */
